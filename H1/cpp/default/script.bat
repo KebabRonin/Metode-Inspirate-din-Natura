18f6a17ec6edd3f957a121751b673851733e47c0
Brassat Alexandru <alrx.brss@gmail.com>
2024-10-31 18:15:04 +0200

    rezultate

---
+++ H1/cpp/default/script.bat
@@ -0,0 +1,2 @@
+.\main.exe A 2 100 100 > outA2_100_100.txt
+.\main.exe H 2 100 100 > outH2_100_100.txt
---
+++ H1/cpp/mutation_decay_and_shuffle/script.bat
@@ -0,0 +1,11 @@
+.\main_mutation_decay_and_shuffle.exe A 2 100 100 > outA2_100_100.txt
+.\main_mutation_decay_and_shuffle.exe H 2 100 100 > outH2_100_100.txt
+
+.\main_mutation_decay_and_shuffle.exe A 30 100 100 > outA30_100_100.txt
+.\main_mutation_decay_and_shuffle.exe H 30 100 100 > outH30_100_100.txt
+
+.\main_mutation_decay_and_shuffle.exe A 100 100 100 > outA100_100_100.txt
+.\main_mutation_decay_and_shuffle.exe A 100 100 300 > outA100_100_300.txt
+.\main_mutation_decay_and_shuffle.exe A 100 1000 300 > outA100_1000_300.txt
+
+.\main_mutation_decay_and_shuffle.exe H 100 100 100 > outH100_100_100.txt
